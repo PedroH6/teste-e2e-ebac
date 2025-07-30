849ec1864438071791c5c49116695815b0ab5289
Pedro Henrique <pedrohenriquebsouza.1@gmail.com>
2025-07-30 17:31:24 -0300

    finalizado trabalho

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import  produtoPage from "../support/page_objects/nome-funcionliada.page"
+
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   /*  Como cliente 
       Quero acessar a Loja EBAC 
@@ -10,12 +12,31 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
+      produtoPage.visitarUrl()
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
+    cy.fixture('produto').then(dados => {
+        produtoPage.buscarProduto(dados[0].nomeProduto)
+        produtoPage.addProdutoCarrinho(dados[0].tamanho, dados[0].Cor, dados[0].quantidade)
+    })
+    cy.get('.woocommerce-message > .button').click()
+    cy.get('.checkout-button').click()
+
+    cy.fixture('checkout').then(dados => {
+        cy.get('#billing_first_name').type(dados[0].nome)
+        cy.get('#billing_last_name').type(dados[0].sobreNome)
+        cy.get('#billing_address_1').type(dados[0].endereço)
+        cy.get('#billing_city').type(dados[0].cidade)
+        cy.get('#billing_postcode').type(dados[0].cep)
+        cy.get('#billing_phone').type(dados[0].telefone)
+        cy.get('#billing_email').type(dados[0].email)
+    })
+    cy.get('#payment_method_cheque').click()
+    cy.get('#terms').click()
+    cy.get('#place_order').click()
+
+    cy.get('.woocommerce-notice').should('exist')
   });
 
 
--- cypress/support/page_objects/nome-funcionliada.page.js
+++ cypress/support/page_objects/nome-funcionliada.page.js
@@ -1,9 +1,23 @@
-class NomeClasse {
+class produtoPage {
 
-    nomeMetodo(parametros ){
-        //ações do método
+     visitarUrl() {
+        cy.visit('produtos')
+    }
+
+    buscarProduto(nomeProduto) {
+        cy.get('.product-block')
+        .contains(nomeProduto)
+        .click()
+    }
+    
+    addProdutoCarrinho(tamnho, cor, quantidade) {
+        cy.get(`.button-variable-item-${tamnho}`).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+
+        cy.get('.single_add_to_cart_button').click() 
     }
 
 }
 
-export default new NomeClasse()
+export default new produtoPage()
